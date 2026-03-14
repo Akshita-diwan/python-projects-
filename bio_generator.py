945e49bffe1410bcd379ac4af3ba117c23c772ed
Akshita-diwan <diwanakshita2005@gmail.com>
2026-03-15 00:22:32 +0530

    Add files via upload

---
+++ bio_generator.py
@@ -0,0 +1,9 @@
+name= input("your name: ")
+profession=input("your profession: ")
+skill=input("your best skill: ")
+city= input("city: ")
+
+bio=f"hi! i am {name},a {profession} from {city} in skill{skill}"
+print("\n bio: ")
+print(bio)
+print(f"\n character count:  {len(bio)}")
